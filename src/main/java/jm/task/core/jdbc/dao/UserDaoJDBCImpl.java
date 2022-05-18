a28c5de8dac2575708df10e9e259b3addbeb8af4
RalfMog <104436602+RalfMog@users.noreply.github.com>
2022-05-18 10:40:57 +0500

    git commit;

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,86 +3,103 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class UserDaoJDBCImpl implements UserDao {
-
-    private final Connection connection = Util.getConnection();
+    private final Connection connection = Util.getInstance().getConnection();
+    private PreparedStatement statement;
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS USER (" +
-                "ID bigint PRIMARY KEY AUTO_INCREMENT NOT NULL,\n" +
-                "NAME varchar(64) NOT NULL,\n" +
-                "LASTNAME varchar(100) NOT NULL,\n" +
-                "AGE tinyint NOT NULL)")) {
+        try {
+            statement = connection.prepareStatement("""
+                    CREATE TABLE IF NOT EXISTS USER (ID bigint PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                    NAME varchar(64) NOT NULL,
+                    LASTNAME varchar(100) NOT NULL,
+                    AGE tinyint NOT NULL)""");
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(statement, connection);
         }
     }
 
     public void dropUsersTable() {
-        try (PreparedStatement statement =
-                     connection.prepareStatement("DROP TABLE IF EXISTS USER")) {
+        try {
+            statement = connection.prepareStatement("DROP TABLE IF EXISTS USER");
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(statement, connection);
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO USER (NAME, LASTNAME, AGE) VALUES(?, ?, ?)")) {
+        try {
+            statement = connection.prepareStatement("INSERT INTO USER (NAME, LASTNAME, AGE) VALUES(?, ?, ?)");
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
-
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(statement, connection);
         }
     }
 
     public void removeUserById(long id) {
-
-        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM USER WHERE ID=?")) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
+        try {
+            statement = connection.prepareStatement("DELETE FROM USER WHERE ID=?");
+            statement.setLong(1, id);
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(statement, connection);
         }
     }
 
     public List<User> getAllUsers() {
         List<User> resultList = new ArrayList<>();
-        try (ResultSet resultSet = connection.prepareStatement("SELECT ID, NAME, LASTNAME, AGE FROM USER")
-                .executeQuery()) {
-            while (resultSet.next()) {
+        ResultSet rs = null;
+        try {
+            statement = connection.prepareStatement("SELECT ID, NAME, LASTNAME, AGE FROM USER");
+            rs = statement.executeQuery();
+            while (rs.next()) {
                 User user = new User();
-                user.setId(resultSet.getLong("ID"));
-                user.setName(resultSet.getString("NAME"));
-                user.setLastName(resultSet.getString("LASTNAME"));
-                user.setAge(resultSet.getByte("AGE"));
+                user.setId(rs.getLong("ID"));
+                user.setName(rs.getString("NAME"));
+                user.setLastName(rs.getString("LASTNAME"));
+                user.setAge(rs.getByte("AGE"));
                 resultList.add(user);
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(rs, statement, connection);
         }
         return resultList;
     }
 
     public void cleanUsersTable() {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("TRUNCATE TABLE USER")) {
-            preparedStatement.executeUpdate();
+        try {
+            statement = connection.prepareStatement("TRUNCATE TABLE USER");
+            statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            Util.closeResources(statement, connection);
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,11 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-
+    private static volatile Util instance;
     private static final String DB_URL = "jdbc:mysql://localhost:3306/my_jdbc";
     private static final String DB_USERNAME = "bestuser";
     private static final String DB_PASSWORD = "bestuser";
     private static Connection connection;
+
+    private Util() {
+    }
+
+    public static Util getInstance() {
+        Util localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Util.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Util();
+                }
+            }
+        }
+        return localInstance;
+    }
 
     public static Connection getConnection() {
         try {
@@ -18,5 +34,18 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static void closeResources(AutoCloseable... resources) {
+        {
+            for (AutoCloseable resource : resources) {
+                if (resource != null) {
+                    try {
+                        resource.close();
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+        }
     }
 }
